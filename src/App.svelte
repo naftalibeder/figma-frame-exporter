00c0425077cf9b6ebd69007f1edeeb4dc7e8d481
Naftali Beder <naftali@naftalibeder.com>
2022-05-10 12:01:04 -0500

    Improve spacing

--- src/App.svelte
+++ src/App.svelte
@@ -351,7 +351,10 @@
   }
   hr {
     width: 100%;
-    border-top: rgb(235, 235, 235);
-    border-width: 1px;
+    margin-top: 2px;
+    margin-bottom: 2px;
+    border-top: 1px rgb(235, 235, 235);
+    border-left: 0px;
+    border-right: 0px;
   }
 </style>
